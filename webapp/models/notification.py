510c19d4c40973dc715669e45e0a3b0d0a52e820
abilash <abilashkj072@gmail.com>
2023-05-01 10:55:28 +0530

    .

--- webapp/models/notification.py
+++ webapp/models/notification.py
@@ -4,7 +4,7 @@ from django.db import models
 class Notifications(models.Model):
     message = models.TextField()
     user = models.ForeignKey("User", on_delete=models.CASCADE)
-    image = models.ImageField(null=True)
+    # image = models.CharField(null=True,max_length=)
     created_at = models.DateTimeField(auto_now_add=True, auto_created=True)
     updated_at = models.DateTimeField(auto_now=True, auto_created=True)
 
