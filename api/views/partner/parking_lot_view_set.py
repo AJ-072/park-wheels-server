51b860f09154e2b0d0dbd9b0e203a92c54ae4675
abilash <abilashkj072@gmail.com>
2023-05-03 00:18:14 +0530

    .

--- api/views/partner/parking_lot_view_set.py
+++ api/views/partner/parking_lot_view_set.py
@@ -18,7 +18,9 @@ class ParkingLotViewSet(ModelViewSet):
         return self.queryset.filter(owner_id=self.request.user.pk)
 
     def create(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=self.request.data, owner=request.user)
+        data = self.request.data;
+        data.append({"owner_id":request.user.pk})
+        serializer = self.serializer_class(data= data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
