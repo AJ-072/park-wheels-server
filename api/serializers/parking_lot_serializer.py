522dae393a4b2578428f893eefbe91f8adfab81f
abilash <abilashkj072@gmail.com>
2023-05-02 22:55:22 +0530

    .

--- api/serializers/parking_lot_serializer.py
+++ api/serializers/parking_lot_serializer.py
@@ -3,6 +3,9 @@ from webapp.models.parking_lot import ParkingLot
 
 
 class ParkingLotSerializer(serializers.ModelSerializer):
+
+    owner = serializers.CurrentUserDefault()
+
     class Meta:
         model = ParkingLot
         fields = "__all__"
