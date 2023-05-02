f8c4936526cbcc17724e9fb3b4b161f2f62daae0
abilash <abilashkj072@gmail.com>
2023-05-02 22:47:59 +0530

    .

--- api/views/client/parking_lot_view_set.py
+++ api/views/client/parking_lot_view_set.py
@@ -1,9 +1,10 @@
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 
 from api.authentication import BearerTokenAuthentication
 from webapp.models import ParkingLot
 from rest_framework.response import Response
-from api.serializers import ParkingLotSerializer, LocationSerializer
+from api.serializers import ParkingLotSerializer, LocationSerializer, SlotSerializer
 from django.contrib.gis.measure import D
 from django.db.models.query import Q
 from rest_framework.viewsets import ReadOnlyModelViewSet
@@ -21,3 +22,10 @@ class ParkingLotViewSet(ReadOnlyModelViewSet):
         location_serializer.is_valid(raise_exception=True)
         query_set = self.get_queryset().filter(location__distance_lte=(location_serializer.point(), D(m=5000)))
         return Response({'result': self.serializer_class(query_set, many=True).data})
+
+    @action("POST", url_path='add-slots', detail=True)
+    def addSlot(self):
+        slotSerializer = SlotSerializer(self.request.data, many=True)
+        slotSerializer.is_valid(raise_exception=True)
+        slotSerializer.save()
+        return Response(status=200, data='successful')
