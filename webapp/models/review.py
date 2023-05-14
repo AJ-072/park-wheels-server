2c01cd2f791a9b64261272489537dc6039dbd64c
abilash <abilashkj072@gmail.com>
2023-05-14 23:36:46 +0530

    .

--- webapp/models/review.py
+++ webapp/models/review.py
@@ -3,9 +3,10 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Review(models.Model):
-    rating = models.DecimalField(max_digits=2, decimal_places=1, default=0,
+    rating = models.DecimalField(max_digits=2, decimal_places=1,
                                  validators=[MaxValueValidator(5), MinValueValidator(0)])
-    comment = models.TextField()
+    title = models.CharField(max_length=100)
+    comment = models.TextField(null=True, blank=True)
     lot = models.ForeignKey("ParkingLot", on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey("User", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, auto_created=True)
