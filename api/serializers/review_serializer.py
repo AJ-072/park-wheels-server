cc51c073632ceeeb8729e69f39deb2db74f0ade3
abilash <abilashkj072@gmail.com>
2023-05-14 23:49:23 +0530

    .

--- api/serializers/review_serializer.py
+++ api/serializers/review_serializer.py
@@ -4,8 +4,6 @@ from webapp.models import Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
-
     class Meta:
         model = Review
         fields = '__all__'
