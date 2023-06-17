d43b5969681016e4989b5e0b2011dacbdd5cf840
abilash <abilashkj072@gmail.com>
2023-06-17 07:42:56 +0530

    .

--- api/views/partner/urls.py
+++ api/views/partner/urls.py
@@ -1,5 +1,6 @@
 from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 from . import BookingView, PartnerViewSet, ParkingLotViewSet, NotificationsViewSet, BookViewSet, SlotViewSet
+from .ai_viewset import AIViewset
 from .review_viewset import ReviewViewSet
 from .template_viewset import TemplateViewSet
 
@@ -8,6 +9,7 @@ partner_router.register(r'template', TemplateViewSet, basename='templates')
 partner_router.register(r'booking', BookingView, basename="Bookings")
 partner_router.register(r'parking-lots', ParkingLotViewSet, basename="lots")
 partner_router.register(r'notifications', NotificationsViewSet, basename="History")
+partner_router.register(r'ai', AIViewset, basename="History")
 partner_router.register(r'', PartnerViewSet, basename="partner")
 parking_lot_router = NestedDefaultRouter(partner_router, r'parking-lots', lookup="parking_lot")
 parking_lot_router.register(r'book', BookViewSet, basename="Book")
